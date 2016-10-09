0ec7b9784cf8ec0dbcccff47efb04ddede7333d5
benoit <benoit.goethals@gmail.com>
2016-10-09 12:55:34 +0200

    int

--- src/main/java/com/example/HomeController.java
+++ src/main/java/com/example/HomeController.java
@@ -1,7 +1,17 @@
 package com.example;
 
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * Created by benoit on 09/10/16.
  */
+@RestController(value = "/home")
 public class HomeController {
+
+    @RequestMapping("/")
+    public String home(@PathVariable String value){
+        return value;
+    }
 }
